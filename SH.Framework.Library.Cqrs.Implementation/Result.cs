36abffa437b6d61c5df9a5a54ce9b9046fc68173
Muharrem Kaçkın <muharrem.kackin@strawhats.company>
2025-11-27 13:02:30 +0300

    result updated for apply openapi and problem details standards

--- SH.Framework.Library.Cqrs.Implementation/Result.cs
+++ SH.Framework.Library.Cqrs.Implementation/Result.cs
@@ -1,67 +1,63 @@
+using System.ComponentModel;
+
 namespace SH.Framework.Library.Cqrs.Implementation;
 
 public class Result<TResponse>: Result
 {
-    public virtual TResponse? Data { get; set; }
+    [ReadOnly(true)]
+    public TResponse? Data { get; init; }
     
     public Result(): base() {}
 }
 
 public class Result
 {
-    public virtual int Code { get; init; }
-    public virtual string? CategorizedCode { get; init; }
-    public virtual string? Description { get; init; }
-    public virtual bool IsSuccess => Code == 0;
-    public virtual bool IsFailure => !IsSuccess;
-    public virtual Dictionary<string, Dictionary<string, string>> Errors { get; init; } = new();
-    public Guid? RequestId { get; set; }
+    [ReadOnly(true)]
+    public int Code { get; init; }
+    [ReadOnly(true)]
+    public string? CategorizedCode { get; init; }
+    [ReadOnly(true)]
+    public string? Description { get; init; }
+    [ReadOnly(true)]
+    public bool IsSuccess => Code == 0;
+    [ReadOnly(true)]
+    public Dictionary<string, string[]> Errors { get; init; } = [];
+    [ReadOnly(true)]
+    public Guid? RequestId { get; init; }
 
     public Result()
     {
     }
 
-    public static Result Success(ResultCode? resultCode = null) => new()
+    public static Result Success(ResultCode? resultCode = null, Guid? requestId = null)
     {
-        Code = resultCode != null ? resultCode.Code : ResultCode.Success.Code,
-        Description = resultCode != null ? resultCode.Description : ResultCode.Success.Description,
-        CategorizedCode = resultCode != null ? resultCode.ToString() : ResultCode.Success.ToString()
-    };
+        resultCode ??= ResultCode.Success;
 
-    public static Result Success(int? code = null, string? description = null) => new()
+        return new()
+        {
+            Code = resultCode.Code,
+            Description = resultCode.Description,
+            CategorizedCode = resultCode.ToString(),
+            RequestId = requestId
+        };
+    }
+
+    
+    public static Result<TResponse> Success<TResponse>(TResponse? data, ResultCode? resultCode = null, Guid? requestId = null)
     {
-        Code = code ?? ResultCode.Success.Code,
-        Description = description ?? ResultCode.Success.Description,
-        CategorizedCode = code != null ? $"CUSTOM{code}" : ResultCode.Success.ToString()
-    };
+        resultCode ??= ResultCode.Success;
 
-    public static Result<TResponse> Success<TResponse>(TResponse? data, ResultCode? resultCode = null) => new()
-    {
-        Code =  resultCode != null ? resultCode.Code : ResultCode.Success.Code,
-        Description = resultCode != null ? resultCode.Description : ResultCode.Success.Description,
-        CategorizedCode = resultCode != null ? resultCode.ToString() : ResultCode.Success.ToString(),
-        Data = data,
-    };
+        return new()
+        {
+            Code = resultCode.Code,
+            Description = resultCode.Description,
+            CategorizedCode = resultCode.ToString(),
+            Data = data,
+            RequestId = requestId
+        };
+    }
 
-    public static Result<TResponse> Success<TResponse>(TResponse? data, int? code = null, string? description = null) => new()
-    {
-        Code = code ?? ResultCode.Success.Code,
-        Description = description ?? ResultCode.Success.Description,
-        CategorizedCode = code != null ? $"CUSTOM{code}" : ResultCode.Success.ToString(),
-        Data = data,
-    };
-
-    public static Result Failure(int code, string description,
-        Dictionary<string, Dictionary<string, string>>? errors = null, Guid? requestId = null) => new()
-    {
-        Code = code,
-        Description = description,
-        Errors = errors ?? [],
-        RequestId = requestId
-    };
-
-    public static Result Failure(ResultCode resultCode, Dictionary<string, Dictionary<string, string>>? errors = null,
-        Guid? requestId = null) => new()
+    public static Result Failure(ResultCode resultCode, Dictionary<string, string[]>? errors = null, Guid? requestId = null) => new()
     {
         Code = resultCode.Code,
         Description = resultCode.Description,
@@ -70,17 +66,7 @@ public class Result
         RequestId = requestId
     };
 
-    public static Result<TResponse> Failure<TResponse>(int code, string description,
-        Dictionary<string, Dictionary<string, string>>? errors = null, Guid? requestId = null) => new()
-        {
-            Code = code,
-            Description = description,
-            Errors = errors ?? [],
-            RequestId = requestId
-        };
-
-    public static Result<TResponse> Failure<TResponse>(ResultCode resultCode,
-        Dictionary<string, Dictionary<string, string>>? errors = null, Guid? requestId = null) => new()
+    public static Result<TResponse> Failure<TResponse>(ResultCode resultCode, Dictionary<string, string[]>? errors = null, Guid? requestId = null) => new()
         {
             Code = resultCode.Code,
             Description = resultCode.Description,
